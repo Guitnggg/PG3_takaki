89f5b5b79b9a2f52f654137aca0ec27ec719577b
k023g0119 <k023g0119@g.neec.ac.jp>
2025-01-14 16:53:00 +0900

    完成

--- PG3_takaki/main.cpp
+++ PG3_takaki/main.cpp
@@ -1,49 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdbool.h>
 #include <Windows.h>
+#include <time.h>
+#include <functional>
 
-// 奇数か偶数かを判定
-bool IsEven(int number) {
-    return number % 2 == 0;
+// コールバック関数
+void DispResult(int* _, int* kye) {
+    int dice = rand() % 2; // 0 or 1 を生成
+
+    if (dice == *kye) {
+        if (dice == 0)
+            printf("%d で丁(偶数)でした。当たり\n", dice);
+        else
+            printf("%d で半(奇数)でした。当たり\n", dice);
+    }
+    else {
+        if (dice == 1)
+            printf("%d で半(奇数)でした。はずれ\n", dice);
+        else
+            printf("%d で丁(偶数)でした。はずれ\n", dice);
+    }
 }
 
-// サイコロを振る関数
-int RollDice() {
-    return rand() % 6 + 1;
-}
+// タイマー関数
+void setTimeout(std::function<void(int*, int*)> p, int seconds, int kye) {
+    // カウントダウン
+    for (int i = seconds; i > 0; i--) {
+        Sleep(1000);
+        printf("%d...\n", i);
+    }
 
-void GuessOddOrEven(bool (*callback)(bool)) {
-    int dice = RollDice();
-    int Guess;
-    bool isEven = IsEven(dice);
-
-    printf("丁（偶数）か半（奇数）か(偶数: 2, 奇数: 1): ");
-    scanf_s("%d", &Guess);
-
-    bool userIsEven = (Guess == 2);
-
-    printf("正解は...\n");
-    Sleep(3000);  // 3秒待つ
-
-    // 結果を表示
-    callback(isEven == userIsEven);
+    // コールバック呼び出し
+    p(nullptr, &kye);
 }
 
 int main() {
-    srand(time(NULL));
+    int kye;
 
-    // ラムダ式を使用してコールバック関数を定義
-    GuessOddOrEven([](bool isCorrect) -> bool {
-        if (isCorrect) {
-            printf("\nおめでとう大正解！\n");
-        }
-        else {
-            printf("\n残念、不正解\n");
-        }
-        return isCorrect;
-        });
+    srand(static_cast<unsigned int>(time(NULL))); // ランダム初期化
+    printf("丁(偶数)なら 0、半(奇数)なら 1 を打つ\n");
+    scanf_s("%d", &kye);
+
+    if (kye == 0) {
+        puts("あなたは丁(偶数)を選びました");
+    }
+    else {
+        puts("あなたは半(奇数)を選びました");
+    }
+
+    // コールバック関数をセット
+    std::function<void(int*, int*)> p = [](int* s, int* kye) {
+        DispResult(s, kye);
+        };
+    setTimeout(p, 3, kye);
 
     return 0;
 }
