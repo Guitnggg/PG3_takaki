cfb5b64bcc512ad43fdb683c546ffb93fff25e83
k023g0119 <k023g0119@g.neec.ac.jp>
2025-01-14 18:41:55 +0900

    完成

--- PG3_takaki/main.cpp
+++ PG3_takaki/main.cpp
@@ -1,11 +1,41 @@
 #include <stdio.h>
-//#include <Windows.h>
+#include <iostream>
+#include <list>
 
-int main()
-{
-	/*SetConsoleOutputCP(65001);*/
+using namespace std;
 
-	printf("ああああ");
+int main() {
+	// 初期の駅リストを定義
+	list<const char*> rosen = { "田端", "日暮里", "西日暮里" };
+
+	// 最初のリストを表示
+	cout << "最初のリスト:" << endl;
+	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
+		cout << *itr << endl;
+	}
+
+	// 駅を追加
+	rosen.push_back("鶯谷");
+	rosen.push_back("上野");
+
+	// 追加後のリストを表示
+	cout << "\n追加後のリスト1:" << endl;
+	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
+		cout << *itr << endl;
+	}
+
+	// 駅をさらに追加
+	rosen.push_back("御徒町");
+	rosen.push_back("秋葉原");
+
+	// さらに追加後のリストを表示
+	cout << "\n追加後のリスト2:" << endl;
+	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
+		cout << *itr << endl;
+	}
+
+	// コンソールウィンドウが閉じないようにする
+	system("pause");
 
 	return 0;
 }
