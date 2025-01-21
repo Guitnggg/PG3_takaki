ee33f60061cc6af88ddbb712bd792172abda3c21
k023g0119 <k023g0119@g.neec.ac.jp>
2025-01-21 10:18:44 +0900

    山手線の全駅のリストに修正

--- PG3_takaki/main.cpp
+++ PG3_takaki/main.cpp
@@ -1,38 +1,56 @@
-#include <stdio.h>
 #include <iostream>
 #include <list>
 
 using namespace std;
 
+void displayList(const list<const char*>& stations, const char* year) {
+    cout << "\n--- Yamanote Line Stations in " << year << " ---" << endl;
+    for (const auto& station : stations) {
+        cout << station << endl;
+    }
+}
+
 int main() {
-	// 初期の駅リストを定義
-	list<const char*> rosen = { "田端", "日暮里", "西日暮里" };
+    // 1970年の山手線のリスト
+    list<const char*> stations1970 = {
+        "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno",
+        "Uguisudani", "Nippori", "Tabata", "Komagome",
+        "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba",
+        "Shin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya",
+        "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
+        "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+    };
 
-	// 最初のリストを表示
-	cout << "最初のリスト:" << endl;
-	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
-		cout << *itr << endl;
-	}
+    // 1970年の駅を表示
+    displayList(stations1970, "1970");
 
-	// 駅を追加
-	rosen.push_back("鶯谷");
-	rosen.push_back("上野");
+	// 西日暮里駅開業に伴い、2019年に西日暮里駅を追加
+    list<const char*> stations2019 = stations1970;
+    auto it2019 = stations2019.begin();
+    for (; it2019 != stations2019.end(); ++it2019) {
+        if (string(*it2019) == "Nippori") {
+            ++it2019; 
+            stations2019.insert(it2019, "Nishi-Nippori");
+            break;
+        }
+    }
 
-	// 追加後のリストを表示
-	cout << "\n追加後のリスト1:" << endl;
-	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
-		cout << *itr << endl;
-	}
+	// 高縄ゲートウェイ駅開業に伴い、2022年に高輪ゲートウェイ駅を追加
+    list<const char*> stations2022 = stations2019;
+    auto it2022 = stations2022.begin();
+    for (; it2022 != stations2022.end(); ++it2022) {
+        if (string(*it2022) == "Tamachi") {
+            ++it2022; 
+            stations2022.insert(it2022, "Takanawa Gateway");
+            break;
+        }
+    }
 
-	// 駅をさらに追加
-	rosen.push_back("御徒町");
-	rosen.push_back("秋葉原");
+    // 2019年の駅を表示
+    displayList(stations2019, "2019");
 
-	// さらに追加後のリストを表示
-	cout << "\n追加後のリスト2:" << endl;
-	for (list<const char*>::iterator itr = rosen.begin(); itr != rosen.end(); itr++) {
-		cout << *itr << endl;
-	}
+	// 2022年の駅を表示
+    displayList(stations2022, "2022");
 
-	return 0;
+    return 0;
 }
