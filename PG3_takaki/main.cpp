bc3e30728d433f80356ac869d5395989e31ddfe5
k023g0119 <k023g0119@g.neec.ac.jp>
2025-01-23 01:14:32 +0900

    完成

--- PG3_takaki/main.cpp
+++ PG3_takaki/main.cpp
@@ -1,11 +1,31 @@
-#include <stdio.h>
-//#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
-int main()
-{
-	/*SetConsoleOutputCP(65001);*/
+std::mutex mtx;
+std::condition_variable cv;
+int current_thread = 1;
 
-	printf("ああああ");
+void printThread(int thread_id, const std::string& message) {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [thread_id]() { return current_thread == thread_id; });
 
-	return 0;
+    std::cout << message << std::endl;
+
+    current_thread++;
+    cv.notify_all();
+}
+
+int main() {
+   
+    std::thread t1(printThread, 1, "thread 1");
+    std::thread t2(printThread, 2, "thread 2");
+    std::thread t3(printThread, 3, "thread 3");
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
 }
