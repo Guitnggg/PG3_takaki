b9d8de7fa8cfa91efcf46f3e41932f60f54849e3
k023g0119 <k023g0119@g.neec.ac.jp>
2025-01-23 01:20:14 +0900

    完成

--- PG3_takaki/main.cpp
+++ PG3_takaki/main.cpp
@@ -1,24 +1,22 @@
-#include <iostream>
+#include <stdio.h>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
 
-std::mutex mtx;
-std::condition_variable cv;
-int current_thread = 1;
+volatile int current_thread = 1;
 
-void printThread(int thread_id, const std::string& message) {
-    std::unique_lock<std::mutex> lock(mtx);
-    cv.wait(lock, [thread_id]() { return current_thread == thread_id; });
+void printThread(int thread_id, const char* message) {
 
-    std::cout << message << std::endl;
+    while (current_thread != thread_id) {
+    
+        std::this_thread::yield();
+    }
+
+    printf("%s\n", message);
 
     current_thread++;
-    cv.notify_all();
 }
 
 int main() {
-   
+
     std::thread t1(printThread, 1, "thread 1");
     std::thread t2(printThread, 2, "thread 2");
     std::thread t3(printThread, 3, "thread 3");
